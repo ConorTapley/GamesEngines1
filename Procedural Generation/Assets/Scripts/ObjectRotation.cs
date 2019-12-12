5a61f777a53a916e574e16b24331d656fc6a2516
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-12-12 13:56:39 -0800

    added rotation to the planet

---
+++ Procedural Generation/Assets/Scripts/ObjectRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectRotation : MonoBehaviour
+{
+    private GameObject planet;
+    [SerializeField] private float rotationSpeed = 2f;
+
+    void Start()
+    {
+        planet = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //planet.transform.eulerAngles = new Vector3(planet.transform.eulerAngles.x, planet.transform.eulerAngles.y * rotationSpeed, planet.transform.eulerAngles.z);
+        planet.transform.Rotate(0, rotationSpeed, 0);
+    }
+}
