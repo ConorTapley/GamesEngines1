848b998d3c9e91beccce7a279245ae908db4d6f0
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2019-12-12 14:58:49 -0800

    Audio Manger

---
+++ Procedural Generation/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
